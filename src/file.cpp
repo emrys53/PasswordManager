543ba352ab01e225ed94e7ff7ff2c9efe56dc3cf
Fikret Ardal <fikret.ardal@tum.de>
2021-12-24 17:56:52 +0100

    license

--- src/file.cpp
+++ src/file.cpp
@@ -1,6 +1,21 @@
-//
-// Created by emrys on 23.12.21.
-//
+/*
+It is a simple Password-Manager written in C++20
+Copyright (C) 2021  Emrys
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 #include "file.h"
 
